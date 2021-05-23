b2c89a6140b1d2d82e41fba8189bec3df621ce81
Francisco De <>
2021-05-23 18:21:30 +0000

    teste switch casa fail

--- main.cpp
+++ main.cpp
@@ -1,15 +1,59 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  char nome[] = "Francisco";
-  int i = 0;
-  while(nome[i])
-  {
-    cout<< nome[i++];
+/*
+struct palavra{
+  string nome;
 
+};
+*/
+char trocaletra(char letra);
+
+int main()
+{
+  char palavra;
+  int i;
+
+  cout<<"Digite a palavra: "<<endl;
+  cin>>palavra;
+
+ 
+  while(palavra!= '\0')
+  {
+    palavra=trocaletra(palavra);
+    
   }
 
-  
+  cout<<palavra<<endl;
+
+  cout<<("\n\n");
   return 0;
+}
+
+char trocaletra(char letra)
+{
+  char novaletra;
+
+  switch (letra)
+  {
+    case 'a':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+      novaletra = 'A';
+      break;
+
+    default:
+      novaletra = letra;
+      break;
+  }
+  return novaletra;
 }

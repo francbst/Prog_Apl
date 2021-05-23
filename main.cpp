5f8710401183079e980a017a11903156f14b3c48
Francisco De <>
2021-05-23 16:41:39 +0000

    Teste string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  
+int main(int argc, char *argv[]) {
+  char nome[] = {'f', 'r', 'a','n', 'c', 'i', 's', 'c', 'o'};
+
+  cout<< nome<<endl;
+  return 0;
 }
